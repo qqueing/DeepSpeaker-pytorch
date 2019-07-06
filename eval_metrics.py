c46d91cb7f35fe9ac2867c4bab93b6dbec8ebca2
Wenhao Ubuntu Lab <874681044@qq.com>
2019-07-06 10:51:47 +0800

    Add EER computation.

--- eval_metrics.py
+++ eval_metrics.py
@@ -12,6 +12,15 @@ def evaluate(distances, labels):
         labels, 1e-3)
     return tpr, fpr, accuracy, val,  far
 
+def evaluate_eer(distances, labels):
+    # Calculate evaluation metrics
+    thresholds = np.arange(0, 30, 0.01)
+    tpr, fpr, fnr, accuracy = calculate_eer(thresholds, distances,
+        labels)
+    thresholds = np.arange(0, 30, 0.001)
+    val,  far = calculate_val(thresholds, distances,
+        labels, 1e-3)
+    return tpr, fpr, fnr, accuracy, val,  far
 
 def calculate_roc(thresholds, distances, labels):
 
@@ -23,7 +32,7 @@ def calculate_roc(thresholds, distances, labels):
     acc_train = np.zeros((nrof_thresholds))
     accuracy = 0.0
 
-    indices = np.arange(nrof_pairs)
+    indices = np.a, range(nrof_pairs)
 
 
     # Find the best threshold for the fold
@@ -31,8 +40,6 @@ def calculate_roc(thresholds, distances, labels):
     for threshold_idx, threshold in enumerate(thresholds):
         tprs[threshold_idx], fprs[threshold_idx], acc_train[threshold_idx] = calculate_accuracy(threshold, distances, labels)
     best_threshold_index = np.argmax(acc_train)
-
-
 
     return tprs[best_threshold_index], fprs[best_threshold_index], acc_train[best_threshold_index]
 
@@ -46,9 +53,52 @@ def calculate_accuracy(threshold, dist, actual_issame):
 
     tpr = 0 if (tp+fn==0) else float(tp) / float(tp+fn)
     fpr = 0 if (fp+tn==0) else float(fp) / float(fp+tn)
+    #fnr = 0 if (tp+fn==0) else float(fn) / float(tp+fn)
+
     acc = float(tp+tn)/dist.size
     return tpr, fpr, acc
 
+
+def calculate_eer(thresholds, distances, labels):
+
+    nrof_pairs = min(len(labels), len(distances))
+    nrof_thresholds = len(thresholds)
+
+    tprs = np.zeros((nrof_thresholds))
+    fprs = np.zeros((nrof_thresholds))
+    fnrs = np.zeros((nrof_thresholds))
+    acc_train = np.zeros((nrof_thresholds))
+    accuracy = 0.0
+
+    indices = np.arange(nrof_pairs)
+
+    # Find the threshold where fnr=fpr for the fold
+    # Todo: And the highest accuracy??
+    eer_index = 0
+    fpr_fnr = 1.0
+    for threshold_idx, threshold in enumerate(thresholds):
+        tprs[threshold_idx], fprs[threshold_idx], fnrs[threshold_idx], acc_train[threshold_idx] = calculate_eer_accuracy(threshold, distances, labels)
+        if np.abs(fprs[threshold_idx]-fnrs[threshold_idx])<fpr_fnr:
+            eer_index = threshold_idx
+            fpr_fnr = np.abs(fprs[threshold_idx]-fnrs[threshold_idx])
+
+    print("Threshold for the eer is {}.".format(thresholds[eer_index]))
+    return tprs[eer_index], fprs[eer_index], fnrs[eer_index], acc_train[eer_index]
+
+
+def calculate_eer_accuracy(threshold, dist, actual_issame):
+    predict_issame = np.less(dist, threshold)
+    tp = np.sum(np.logical_and(predict_issame, actual_issame))
+    fp = np.sum(np.logical_and(predict_issame, np.logical_not(actual_issame)))
+    tn = np.sum(np.logical_and(np.logical_not(predict_issame), np.logical_not(actual_issame)))
+    fn = np.sum(np.logical_and(np.logical_not(predict_issame), actual_issame))
+
+    tpr = 0 if (tp + fn == 0) else float(tp) / float(tp + fn)
+    fpr = 0 if (fp + tn == 0) else float(fp) / float(fp + tn)
+    fnr = 0 if (tp + fn == 0) else float(fn) / float(tp + fn)
+
+    acc = float(tp + tn) / dist.size
+    return tpr, fpr, fnr, acc
 
 def calculate_val(thresholds, distances, labels, far_target=0.1):
     nrof_pairs = min(len(labels), len(distances))
@@ -69,7 +119,6 @@ def calculate_val(thresholds, distances, labels, far_target=0.1):
         threshold = 0.0
 
     val, far = calculate_val_far(threshold, distances, labels)
-
 
     return val, far
 
