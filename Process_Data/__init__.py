91ba8fb548e3573b37c3a0cffa1de9fa69855c2e
MacWilliam <874681044@qq.com>
2019-10-20 14:30:38 +0800

    add make vox1 from kaldi

---
+++ Process_Data/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@Author: yangwenhao
+@Contact: 874681044@qq.com
+@Software: PyCharm
+@File: __init__.py.py
+@Time: 2019/10/20 下午2:27
+@Overview:
+"""
