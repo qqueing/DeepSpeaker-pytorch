d35ad0361d1dbc38db5f25dd0400d20dde2843b0
MacWilliam <874681044@qq.com>
2020-02-01 12:31:26 +0800

    __init__ add

---
+++ Define_Model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@Author: yangwenhao
+@Contact: 874681044@qq.com
+@Software: PyCharm
+@File: __init__.py
+@Time: 2019/9/13 下午4:58
+@Overview:
+"""
+__all__ = ['CenterLoss', 'model', 'ResNet', 'TDNN', 'SoftmaxLoss']
