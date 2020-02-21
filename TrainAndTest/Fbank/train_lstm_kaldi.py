e6eab8f9668f66334e494de9c3bcd8ce1ece7264
MacWilliam <874681044@qq.com>
2020-02-21 21:29:57 +0800

    rnn varlength

--- TrainAndTest/Fbank/train_lstm_kaldi.py
+++ TrainAndTest/Fbank/train_lstm_kaldi.py
@@ -83,7 +83,7 @@ parser.add_argument('--epochs', type=int, default=22, metavar='E',
 parser.add_argument('--cos-sim', action='store_true', default=True,
                     help='using Cosine similarity')
 
-parser.add_argument('--batch-size', type=int, default=80, metavar='BS',
+parser.add_argument('--batch-size', type=int, default=128, metavar='BS',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--test-batch-size', type=int, default=16, metavar='BST',
                     help='input batch size for testing (default: 64)')
@@ -98,7 +98,7 @@ parser.add_argument('--margin', type=float, default=3, metavar='MARGIN',
 parser.add_argument('--loss-ratio', type=float, default=2.0, metavar='LOSSRATIO',
                     help='the ratio softmax loss - triplet loss (default: 2.0')
 
-parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
                     help='learning rate (default: 0.125)')
 parser.add_argument('--lr-decay', default=0.01, type=float, metavar='LRD',
                     help='learning rate decay ratio (default: 1e-4')
