bbfe53328549c6f03d26de33486b4067cad7d889
MacWilliam <874681044@qq.com>
2020-02-26 23:17:26 +0800

    rnn varlength

--- TrainAndTest/Fbank/train_lstm_tuple.py
+++ TrainAndTest/Fbank/train_lstm_tuple.py
@@ -231,7 +231,7 @@ def main():
     criterion = [nn.CrossEntropyLoss().cuda(), TupleLoss(args.batch_size, args.tuple_size)]
 
     for epoch in range(start, end):
-        # pdb.set_trace()
+        pdb.set_trace()
         # compute_dropout(model, optimizer, epoch, end)
         train(train_loader, model, optimizer, criterion, epoch)
         test(test_loader, valid_loader, model, epoch)
