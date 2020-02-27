7029cb49e92a1b618be654abad04303a5089c794
MacWilliam <874681044@qq.com>
2020-02-28 01:16:43 +0800

    input dim

--- TrainAndTest/Fbank/train_tdnn_kaldi.py
+++ TrainAndTest/Fbank/train_tdnn_kaldi.py
@@ -188,7 +188,7 @@ def main():
 
     # instantiate
     # model and initialize weights
-    model = XVectorTDNN(len(train_dir.speakers), dropout_p=0.2)
+    model = XVectorTDNN(len(train_dir.speakers), input_dim=args.feat_dim, dropout_p=0.2)
 
     if args.cuda:
         model.cuda()
