c7e37cf5f86f8b588478029a12029aa6b1ec2975
MacWilliam <874681044@qq.com>
2020-03-02 01:00:07 +0800

    interval

--- Process_Data/Compute-feat/make_spectrogram.py
+++ Process_Data/Compute-feat/make_spectrogram.py
@@ -105,7 +105,8 @@ class MakeFeatsProcess(Process):
             pair = wav.split()
             compute_wav_path(pair, self.feat_scp, self.feat_ark)
             self.queue.put(pair[0])
-            print(self.queue.qsize())
+            if self.queue.qsize() % 100 == 0:
+                print(self.queue.qsize())
 
         print('>>Process {} finished!'.format(self.proid))
 
