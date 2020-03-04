b0a253c82886a8c0fd277b282e7b926b0acc159c
MacWilliam <874681044@qq.com>
2020-03-04 17:30:59 +0800

    alstm

--- Define_Model/model.py
+++ Define_Model/model.py
@@ -745,3 +745,64 @@ class LSTM_End(nn.Module):
 
         return spk_vec, logits
 
+
+class AttentionLSTM(nn.Module):
+    def __init__(self, input_dim, num_class, batch_size,
+                 hidden_shape=128, project_dim=64, num_lstm=2,
+                 dropout_p=0.2, attention_dim=64):
+        super(AttentionLSTM, self).__init__()
+
+        self.num_lstm = num_lstm
+        self.hidden_shape = hidden_shape
+        self.lstm_layer = nn.LSTM(input_size=input_dim,
+                                  hidden_size=hidden_shape,
+                                  num_layers=self.num_lstm,
+                                  batch_first=True,
+                                  dropout=dropout_p)
+
+        self.h0 = torch.rand(self.num_lstm, batch_size, hidden_shape)
+        self.c0 = torch.rand(self.num_lstm, batch_size, hidden_shape)
+
+        self.relu = ReLU(inplace=True)
+        self.dropout = nn.Dropout(p=dropout_p)
+
+        self.attention_linear = nn.Linear(hidden_shape, hidden_shape)
+        self.attention_activation = nn.Sigmoid()
+        self.attention_vector = nn.Parameter(torch.rand(hidden_shape, 1))
+        self.attention_soft = nn.Tanh()
+
+        self.fc1 = nn.Linear(hidden_shape, project_dim)
+        self.bn1 = nn.BatchNorm1d(project_dim)
+
+        self.fc2 = nn.Linear(project_dim, num_class)
+
+    def attention_layer(self, x):
+        """
+        :param x:   [length,feat_dim] vector
+        :return:   [feat_dim] vector
+        """
+        fx = self.attention_activation(self.attention_linear(x))
+        vf = fx.matmul(self.attention_vector)
+        alpha = self.attention_soft(vf)
+
+        alpha_ht = x.mul(alpha)
+        w = torch.sum(alpha_ht, dim=-2)
+
+        return w
+
+    def forward(self, input):
+        """
+        :param input: should be features with fixed length
+        :return:
+        """
+
+        out, (_, _) = self.lstm_layer(input, (self.h0, self.c0))
+        rnn_out = self.attention_layer(out)
+        # rnn_last =
+        spk_vec = self.fc1(rnn_out)
+        spk_vec = self.dropout(spk_vec)
+        spk_vec = self.relu(self.bn1(spk_vec))
+
+        logits = self.fc2(spk_vec)
+
+        return spk_vec, logits
