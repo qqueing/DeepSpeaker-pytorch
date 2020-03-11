90aa67ae45b353488b37d92d45c074a319f2b14f
MacWilliam <874681044@qq.com>
2020-03-11 22:34:19 +0800

    sample length => 600

--- Process_Data/constants.py
+++ Process_Data/constants.py
@@ -2,7 +2,7 @@ DATASET_DIR = '../Data/dataset/'
 AUDIO_DIR = '../Data/dataset/'
 
 # The length of shortest wav
-N_SAMPLES = 500
+N_SAMPLES = 600
 MINIMUIN_LENGTH = 400
 
 # Parameters for fbank features
