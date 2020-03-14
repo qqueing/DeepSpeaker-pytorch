b88d99c66c8904832c06d876a9e48d83fa789c38
MacWilliam <874681044@qq.com>
2020-03-14 23:09:30 +0800

    sitw_eval_test

--- Process_Data/Compute_Feat/make_spectrogram_npy.py
+++ Process_Data/Compute_Feat/make_spectrogram_npy.py
@@ -83,10 +83,10 @@ if __name__ == "__main__":
     parser.add_argument('--nj', type=int, default=4, metavar='E',
                         help='number of jobs to make feats (default: 10)')
     parser.add_argument('--data-dir', type=str,
-                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/sitw/sitw_eval_enroll',
+                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/sitw/sitw_eval_test',
                         help='number of jobs to make feats (default: 10)')
     parser.add_argument('--out-dir', type=str,
-                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/sitw_spect/sitw_eval_enroll',
+                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/sitw_spect/sitw_eval_test',
                         help='number of jobs to make feats (default: 10)')
 
     parser.add_argument('--conf', type=str, default='condf/spect.conf', metavar='E',
