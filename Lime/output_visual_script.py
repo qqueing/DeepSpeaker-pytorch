007aec22a51c0e8266ecdbf049a9ece07c0344d2
MacWilliam <874681044@qq.com>
2020-03-22 17:16:50 +0800

    pdb

--- Lime/output_visual_script.py
+++ Lime/output_visual_script.py
@@ -68,7 +68,7 @@ def main():
 
         conv1_epoch = np.load(str(conv1s[0])).squeeze()
         conv1_epoch = conv1_epoch[np.newaxis, :]
-        # pdb.set_trace()
+        pdb.set_trace()
         conv1s = np.concatenate((conv1s, conv1_epoch), axis=0)
 
     means = np.mean(np.abs(conv1s), axis=(2, 3))
