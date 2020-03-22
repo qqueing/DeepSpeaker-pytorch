4ed2ca10c4e3a071590c380c2e37d88766de3907
MacWilliam <874681044@qq.com>
2020-03-23 01:09:04 +0800

    vidual siresnet34 pdb

--- Lime/sires_visual_script.py
+++ Lime/sires_visual_script.py
@@ -98,6 +98,8 @@ def main():
     means = np.mean(np.abs(conv1s), axis=(2, 3))
     stds = np.std(conv1s, axis=(2, 3))
 
+    pdb.set_trace()
+
     plt.switch_backend('agg')
     fig, ax = plt.subplots(figsize=(8, 8))
     plt.title('Convergence of 64 Filters 0-20 Epochs')
