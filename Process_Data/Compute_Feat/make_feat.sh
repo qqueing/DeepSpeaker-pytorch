04de009c02b6d731d7e5cd2796e4c3c2e8cc99d5
MacWilliam <874681044@qq.com>
2020-04-03 02:25:03 +0800

    make sh

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -2,7 +2,7 @@
 
 for name in reverb babble noise music ; do
   python Process_Data/Compute_Feat/make_feat_kaldi.py \
-    --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_${name}_fb64/dev \
-    --out-set dev_$(name}
+  --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_${name}_fb64/dev \
+  --out-set dev_${name}
 
 done
