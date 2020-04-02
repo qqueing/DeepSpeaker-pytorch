435d5c1e90f4fd0521ed9aca00092a50b929de58
MacWilliam <874681044@qq.com>
2020-04-03 01:51:25 +0800

    vox1 test

--- Process_Data/Compute_Feat/make_feat_kaldi.py
+++ Process_Data/Compute_Feat/make_feat_kaldi.py
@@ -105,13 +105,12 @@ if __name__ == "__main__":
     parser.add_argument('--nj', type=int, default=15, metavar='E',
                         help='number of jobs to make feats (default: 10)')
     parser.add_argument('--data-dir', type=str,
-                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev',
+                        default='/home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test',
                         help='number of jobs to make feats (default: 10)')
     parser.add_argument('--out-dir', type=str,
                         default='/home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect',
                         help='number of jobs to make feats (default: 10)')
-    parser.add_argument('--out-set', type=str,
-                        default='dev',
+    parser.add_argument('--out-set', type=str, default='test',
                         help='number of jobs to make feats (default: 10)')
 
     parser.add_argument('--conf', type=str, default='condf/spect.conf', metavar='E',
