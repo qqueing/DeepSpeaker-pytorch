f73b8e4570c4f99455f63955035f1b976d1a39b7
MacWilliam <874681044@qq.com>
2020-04-03 13:35:10 +0800

    make preemph sepect

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-for name in reverb babble noise music ; do
-  python Process_Data/Compute_Feat/make_feat_kaldi.py \
-  --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_${name}_fb64/dev \
-  --out-set dev_${name}
+stage=0
 
-done
+if [ $stage -le 0 ]; then
+  for name in dev test ; do
+    python Process_Data/Compute_Feat/make_feat_kaldi.py \
+      --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_fb64/${name} \
+      --out-set ${name}
+
+  done
+fi
+
+if [ $stage -le 1 ]; then
+  for name in reverb babble noise music ; do
+    python Process_Data/Compute_Feat/make_feat_kaldi.py \
+      --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_${name}_fb64/dev \
+      --out-set dev_${name}
+
+  done
+fi
+
