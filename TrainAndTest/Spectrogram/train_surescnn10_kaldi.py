59d9aeaf8370b7a8885af1c3032da4f06d30ddba
MacWilliam <874681044@qq.com>
2020-04-03 18:13:13 +0800

    float32 exres shape1

--- TrainAndTest/Spectrogram/train_surescnn10_kaldi.py
+++ TrainAndTest/Spectrogram/train_surescnn10_kaldi.py
@@ -281,7 +281,7 @@ def main():
         for param_group in optimizer.param_groups:
             print('\n\33[1;34m Current \'{}\' learning rate is {}.\33[0m'.format(args.optimizer, param_group['lr']))
 
-        # train(train_loader, model, ce, optimizer, scheduler, epoch)
+        train(train_loader, model, ce, optimizer, scheduler, epoch)
         test(test_loader, valid_loader, model, epoch)
         # sitw_test(sitw_test_loader, model, epoch)
         # sitw_test(sitw_dev_loader, model, epoch)
