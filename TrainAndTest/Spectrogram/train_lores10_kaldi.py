a2a4365551baba09c42e41bad331569ff80b6567
MacWilliam <874681044@qq.com>
2020-04-05 01:43:58 +0800

    Combine all loss to one lores.py remove logger sample 224

--- TrainAndTest/Spectrogram/train_lores10_kaldi.py
+++ TrainAndTest/Spectrogram/train_lores10_kaldi.py
@@ -98,7 +98,7 @@ parser.add_argument('--embedding-size', type=int, default=1024, metavar='ES',
                     help='Dimensionality of the embedding')
 parser.add_argument('--batch-size', type=int, default=128, metavar='BS',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--input-per-spks', type=int, default=192, metavar='IPFT',
+parser.add_argument('--input-per-spks', type=int, default=224, metavar='IPFT',
                     help='input sample per file for testing (default: 8)')
 parser.add_argument('--test-input-per-file', type=int, default=4, metavar='IPFT',
                     help='input sample per file for testing (default: 8)')
