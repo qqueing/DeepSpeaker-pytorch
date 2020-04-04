76f5d346b329c7b397d9ead2e22578d2ae339b2b
MacWilliam <874681044@qq.com>
2020-04-04 11:20:14 +0800

    Bottleblock

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -21,6 +21,7 @@ from torchvision.models.resnet import Bottleneck
 from torchvision.models.resnet import BasicBlock
 
 from Define_Model.SoftmaxLoss import AngleLinear
+from Define_Model.model import ReLU
 
 
 def conv1x1(in_planes, out_planes, stride=1):
@@ -56,8 +57,7 @@ class SimpleResNet(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(1, num_filter[0], kernel_size=3, stride=1, padding=1,
-                               bias=False)
+        self.conv1 = nn.Conv2d(1, num_filter[0], kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = norm_layer(num_filter[0])
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
@@ -633,3 +633,157 @@ class AResNet(nn.Module):
 # x_vectors = model.pre_forward(input)
 # outputs = model(x_vectors)
 # print('hello')
+
+class ResNet20(nn.Module):
+    def __init__(self, layers, block=BasicBlock,
+                 input_frames=300,
+                 num_classes=1000,
+                 embedding_size=128,
+                 zero_init_residual=False,
+                 groups=1,
+                 width_per_group=64,
+                 replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet20, self).__init__()
+
+        self.layer1 = self._make_layer(Bottleneck, 64, 1, stride=2)
+        self.layer2 = self._make_layer(Bottleneck, 128, 1, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 128, 1)
+        self.layer4 = self._make_layer(Bottleneck, 256, 1, stride=2)
+        self.layer5 = self._make_layer(BasicBlock, 256, 3)
+        self.layer6 = self._make_layer(Bottleneck, 512, 1, stride=2)
+
+        self.avg = nn.AdaptiveAvgPool2d(1, None)
+        fc5 = nn.Linear()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+
+class LocalResNet(nn.Module):
+    """
+    Define the ResNet model with A-softmax and AM-softmax loss.
+    Added dropout as https://github.com/nagadomi/kaggle-cifar10-torch7 after average pooling and fc layer.
+    """
+
+    def __init__(self, resnet_size, embedding_size, num_classes, block=Bottleneck, feature_dim=64, dropout=False):
+        super(LocalResNet, self).__init__()
+        resnet_type = {10: [1, 1, 1, 1],
+                       18: [2, 2, 2, 2],
+                       34: [3, 4, 6, 3],
+                       50: [3, 4, 6, 3],
+                       101: [3, 4, 23, 3]}
+
+        layers = resnet_type[resnet_size]
+
+        self.embedding_size = embedding_size
+        self.relu = nn.LeakyReLU()
+        # self.relu = ReLU(inplace=True)
+
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=2, padding=2, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+
+        self.in_planes = 128
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.layer2 = self._make_layer(block, 128, layers[1])
+
+        self.in_planes = 256
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2, bias=False)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.layer3 = self._make_layer(block, 256, layers[2])
+
+        # self.in_planes = 512
+        # self.conv4 = nn.Conv2d(256, 512, kernel_size=5, stride=2, padding=2, bias=False)
+        # self.bn4 = nn.BatchNorm2d(512)
+        # self.layer4 = self._make_layer(block, 512, layers[3])
+        # self.avg_pool = nn.AdaptiveAvgPool2d([4, 1])
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 8))
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.in_planes * 8, embedding_size),
+            nn.BatchNorm1d(embedding_size)
+        )
+        self.classifier = nn.Linear(self.embedding_size, num_classes)
+
+        for m in self.modules():  # 对于各层参数的初始化
+            if isinstance(m, nn.Conv2d):  # 以2/n的开方为标准差，做均值为0的正态分布
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):  # weight设置为1，bias为0
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):  # weight设置为1，bias为0
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+        # self.weight = nn.Parameter(torch.Tensor(embedding_size, num_classes))  # 本层权重
+        # self.weight.data.uniform_(-1, 1).renorm_(2, 1, 1e-5).mul_(1e5)  # 初始化权重，在第一维度上做normalize
+
+    def l2_norm(self, input):
+        input_size = input.size()
+        buffer = torch.pow(input, 2)
+
+        normp = torch.sum(buffer, 1).add_(1e-10)
+        norm = torch.sqrt(normp)
+
+        _output = torch.div(input, norm.view(-1, 1).expand_as(input))
+        output = _output.view(input_size)
+
+        return output
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        layers = [block(self.in_planes, planes, stride)]
+        self.in_planes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.layer3(x)
+
+        # x = self.conv4(x)
+        # x = self.bn4(x)
+        # x = self.relu(x)
+        # x = self.layer4(x)
+
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        # x = self.l2_norm(x)
+        # # Multiply by alpha = 10 as suggested in https://arxiv.org/pdf/1703.09507.pdf
+        # alpha = 10
+        # feature = x * alpha
+        logits = self.classifier(x)
+
+        # res = self.model.classifier(features)
+        return logits, x
