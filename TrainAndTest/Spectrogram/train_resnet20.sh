a9459f42f26d79e3e2b4873663fc16c3e438ff0f
MacWilliam <874681044@qq.com>
2020-04-08 01:52:33 +0800

    ark dir

--- TrainAndTest/Spectrogram/train_resnet20.sh
+++ TrainAndTest/Spectrogram/train_resnet20.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+waited=0
 while [ `ps 160299 | wc -l` -eq 2 ]; do
-    echo -e "\033[1;4;31m Waiting for 10s!\033[0m\r"
+    echo -e "\f\033[1;4;31m Having waited for ${waited}s!\033[0m\c"
+    waited=$(expr $waited + 10)
     sleep 10
 #    break
 done
