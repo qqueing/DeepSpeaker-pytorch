ab4a4ccdfc664591f429f904815d97bc7eaf6b58
MacWilliam <874681044@qq.com>
2020-04-07 21:43:33 +0800

    ark dir

--- TrainAndTest/Spectrogram/train_resnet20.sh
+++ TrainAndTest/Spectrogram/train_resnet20.sh
@@ -6,7 +6,7 @@ if [ $stage -le 0 ]; then
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
     python TrainAndTest/Spectrogram/train_resnet20_kaldi.py \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev_257 \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_257 \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test_257 \
       --embedding-size 128 \
       --batch-size 64 \
       --nj 12 \
