11f972c37370ed27023a72c2f8a71ec7c08bce41
MacWilliam <874681044@qq.com>
2020-04-07 20:24:47 +0800

    ResNet20

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -28,6 +28,12 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, padding=1,
+                     stride=stride, bias=False)
+
 class SimpleResNet(nn.Module):
 
     def __init__(self, layers, block=BasicBlock,
@@ -630,27 +636,78 @@ class AResNet(nn.Module):
 # outputs = model(x_vectors)
 # print('hello')
 
+# M. Hajibabaei and D. Dai, “Unified hypersphere embedding for speaker recognition,”
+# arXiv preprint arXiv:1807.08312, 2018.
+class Block3x3(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Block3x3, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.conv3 = conv3x3(planes, planes)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
 class ResNet20(nn.Module):
-    def __init__(self, layers, block=BasicBlock,
-                 input_frames=300,
-                 num_classes=1000,
-                 embedding_size=128,
-                 zero_init_residual=False,
-                 groups=1,
-                 width_per_group=64,
-                 replace_stride_with_dilation=None,
-                 norm_layer=None):
+    def __init__(self, layers, block=BasicBlock, input_frames=300, num_classes=1000, embedding_size=128,
+                 zero_init_residual=False):
         super(ResNet20, self).__init__()
+        self.inplanes = 1
+        self.layer1 = self._make_layer(Block3x3, planes=64, blocks=1, stride=2)
 
-        self.layer1 = self._make_layer(Bottleneck, 64, 1, stride=2)
-        self.layer2 = self._make_layer(Bottleneck, 128, 1, stride=2)
+        self.inplanes = 64
+        self.layer2 = self._make_layer(Block3x3, planes=128, blocks=1, stride=2)
+
+        self.inplanes = 128
         self.layer3 = self._make_layer(BasicBlock, 128, 1)
-        self.layer4 = self._make_layer(Bottleneck, 256, 1, stride=2)
-        self.layer5 = self._make_layer(BasicBlock, 256, 3)
-        self.layer6 = self._make_layer(Bottleneck, 512, 1, stride=2)
 
-        self.avg = nn.AdaptiveAvgPool2d(1, None)
-        fc5 = nn.Linear()
+        self.inplanes = 128
+        self.layer4 = self._make_layer(Block3x3, planes=256, blocks=1, stride=2)
+
+        self.inplanes = 256
+        self.layer5 = self._make_layer(BasicBlock, 256, 3)
+
+        self.inplanes = 256
+        self.layer6 = self._make_layer(Block3x3, planes=512, blocks=1, stride=2)
+
+        self.inplanes = 512
+        self.avgpool = nn.AdaptiveAvgPool2d(1, None)
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(17 * self.inplanes, embedding_size),
+            nn.BatchNorm1d(embedding_size)
+        )
+        self.classifier = nn.Linear(embedding_size, num_classes)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -667,6 +724,23 @@ class ResNet20(nn.Module):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        feat = self.fc1(x)
+
+        logits = self.classifier(feat)
+
+        return logits, feat
 
 
 class LocalResNet(nn.Module):
