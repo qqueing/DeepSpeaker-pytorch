1cc01fa87d86d1f3b72ce31d6ee78d8cc64ab316
MacWilliam <874681044@qq.com>
2020-04-07 19:32:57 +0800

    ark dir

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=2
+stage=1
 #stage=10
 
 if [ $stage -le 0 ]; then
@@ -25,7 +25,7 @@ fi
 
 if [ $stage -le 1 ]; then
 #  for loss in center amsoft ; do/
-  for loss in amsoft center ; do
+  for loss in center ; do
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
@@ -46,6 +46,7 @@ if [ $stage -le 1 ]; then
   done
 
 fi
+stage=10
 
 if [ $stage -le 2 ]; then
 #  for loss in center amsoft ; do/
