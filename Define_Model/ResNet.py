5755487cb7f88ac50d821aa478f3f50a2d8529d1
MacWilliam <874681044@qq.com>
2020-04-07 20:31:54 +0800

    ark dir

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -31,7 +31,7 @@ def conv1x1(in_planes, out_planes, stride=1):
 
 def conv3x3(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, padding=1,
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1,
                      stride=stride, bias=False)
 
 class SimpleResNet(nn.Module):
