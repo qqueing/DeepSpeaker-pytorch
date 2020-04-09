284d5c7d8cf48e3cc9b3e70fed59a83ff9d0fe5a
MacWilliam <874681044@qq.com>
2020-04-09 18:40:58 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -1,75 +1,60 @@
 #!/usr/bin/env bash
 
 #stage=3
-stage=1
+stage=0
 
 if [ $stage -le 0 ]; then
-  for loss in soft  ; do
+  for loss in soft ; do
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
-      --nj 12 \
-      --epochs 12 \
-      --lr 0.05 \
-      --milestones 6,10 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_lr \
-      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}_lr/checkpoint_1.pth \
+      --nj 14 \
+      --epochs 15 \
+      --lr 0.1 \
+      --milestones 7,11 \
+      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss} \
+      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}/checkpoint_1.pth \
       --channels 4,16,64 \
       --embedding-size 128 \
       --input-per-spks 256 \
       --num-valid 2 \
       --weight-decay 0.001 \
+      --dropout-p 0.5 \
       --loss-type ${loss}
   done
 fi
 
 if [ $stage -le 1 ]; then
-  for loss in soft  ; do
+  for loss in asoft amsoft center ; do
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
       --nj 12 \
-      --epochs 12 \
-      --lr 0.05 \
-      --milestones 6,10 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_wd \
-      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}_wd/checkpoint_1.pth \
+      --epochs 8 \
+      --lr 0.1 \
+      --loss-ratio 0.01 \
+      --milestones 3,6 \
+      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss} \
+      --resume Data/checkpoint/LoResNet10/timit_spect/soft/checkpoint_1.pth \
       --channels 4,16,64 \
       --embedding-size 128 \
       --input-per-spks 256 \
       --num-valid 2 \
       --dropout-p 0.5 \
-      --weight-decay 0.0005 \
-      --loss-type ${loss}
-  done
-fi
-stage=13
-if [ $stage -le 0 ]; then
-  for loss in soft amsoft ; do
-    echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
-      --nj 12 \
-      --epochs 16 \
-      --lr 0.05 \
-      --milestones 6,12 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_epoch \
-      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}_epoch/checkpoint_1.pth \
-      --channels 4,16,64 \
-      --embedding-size 128 \
-      --input-per-spks 256 \
-      --num-valid 2 \
       --weight-decay 0.001 \
-      --loss-type ${loss}
+      --loss-type ${loss} \
+      --m 4 \
+      --lambda-max 0.2 \
+      --margin 0.3 \
+      --s 30
   done
 fi
 
 
-
-if [ $stage -le 1 ]; then
+stage=2
+if [ $stage -le 2 ]; then
 #  for loss in center amsoft amsoft ; do/
   for loss in amsoft ; do
     echo -e "\n\033[1;4;31m Finetuning with ${loss}\033[0m\n"
@@ -78,15 +63,13 @@ if [ $stage -le 1 ]; then
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
       --nj 12 \
       --epochs 6 \
-      --scheduler exp \
-      --gamma 0.625 \
       --milestones 4 \
       --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_sch5 \
       --resume Data/checkpoint/LoResNet10/timit_spect/soft/checkpoint_12.pth \
       --channels 4,16,64 \
       --embedding-size 128 \
       --input-per-spks 256 \
-      --lr 0.05 \
+      --lr 0.1 \
       --loss-ratio 0.01 \
       --num-valid 2 \
       --weight-decay 0.001 \
@@ -96,22 +79,19 @@ if [ $stage -le 1 ]; then
       --margin 0.3 \
       --s 30
   done
-
 fi
 
-
-
-if [ $stage -le 2 ]; then
+if [ $stage -le 3 ]; then
 #  for loss in center amsoft ; do/
-  for kernel in '3,7' '3,5' '5,7' '5,3' '7,3' ; do
+  for kernel in '3,3' '3,5' ; do
     echo -e "\n\033[1;4;31m Training with kernel size ${kernel} \033[0m\n"
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
       --nj 12 \
-      --epochs 12 \
-      --milestones 6,10 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss} \
+      --epochs 15 \
+      --lr 0.1 \
+      --milestones 7,11 \
       --resume None \
       --channels 4,16,64 \
       --embedding-size 128 \
@@ -121,10 +101,10 @@ if [ $stage -le 2 ]; then
       --weight-decay 0.001 \
       --kernel-size ${kernel}
   done
-
 fi
 
-if [ $stage -le 3 ]; then
+stage=5
+if [ $stage -le 4 ]; then
 #  for loss in center amsoft ; do/
 #  for p in 0.1 0.2 0.5 ; do
   for p in 0.0 0.1 0.2 0.5 ; do
@@ -148,23 +128,4 @@ if [ $stage -le 3 ]; then
 
 fi
 
-if [ $stage -le 4 ]; then
-  for loss in soft asoft; do
-    echo -e "\n\033[1;4;31m Training with ${loss} with dropout 0.5 \033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect \
-      --nj 12 \
-      --epochs 12 \
-      --milestones 6,10 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_dp0.5 \
-      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}/checkpoint_1.pth \
-      --channels 4,16,64 \
-      --embedding-size 128 \
-      --input-per-spks 256 \
-      --num-valid 2 \
-      --weight-decay 0.001 \
-      --loss-type ${loss} \
-      --dropout-p 0.5
-  done
-fi
+exit 0;
