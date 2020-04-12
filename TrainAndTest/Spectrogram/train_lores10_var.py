d3d762da7ae59eeb442bc1d3526be6514de57480
MacWilliam <874681044@qq.com>
2020-04-13 04:08:29 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores10_var.py
+++ TrainAndTest/Spectrogram/train_lores10_var.py
@@ -36,7 +36,7 @@ from Define_Model.ResNet import LocalResNet
 from Define_Model.SoftmaxLoss import AngleSoftmaxLoss, AngleLinear, AdditiveMarginLinear, AMSoftmaxLoss
 from Define_Model.model import PairwiseDistance
 from Process_Data.KaldiDataset import ScriptTrainDataset, ScriptTestDataset, ScriptValidDataset
-from Process_Data.audio_processing import to2tensor, concateinputfromMFB
+from Process_Data.audio_processing import to2tensor, concateinputfromMFB, varLengthFeat, PadCollate
 from Process_Data.audio_processing import toMFB, totensor, truncatedinput, read_audio
 from TrainAndTest.common_func import create_optimizer
 from eval_metrics import evaluate_kaldi_eer, evaluate_kaldi_mindcf
@@ -210,8 +210,8 @@ l2_dist = nn.CosineSimilarity(dim=1, eps=1e-6) if args.cos_sim else PairwiseDist
 
 if args.acoustic_feature == 'fbank':
     transform = transforms.Compose([
-        concateinputfromMFB(num_frames=c.NUM_FRAMES_SPECT, remove_vad=False),
-        # varLengthFeat(),
+        # concateinputfromMFB(num_frames=c.NUM_FRAMES_SPECT, remove_vad=False),
+        varLengthFeat(),
         to2tensor()
     ])
     transform_T = transforms.Compose([
@@ -343,7 +343,7 @@ def main():
     end = start + args.epochs
 
     train_loader = torch.utils.data.DataLoader(train_dir, batch_size=args.batch_size,
-                                               # collate_fn=PadCollate(dim=2, normlize=args.normlize, fix_len=False),
+                                               collate_fn=PadCollate(dim=2, fix_len=False),
                                                shuffle=True, **kwargs)
     valid_loader = torch.utils.data.DataLoader(valid_dir, batch_size=int(args.batch_size / 2),
                                                # collate_fn=PadCollate(dim=2, normlize=args.normlize, fix_len=False),
