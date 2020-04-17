00730c23d2ad8d7d9abda21be6a57e426bbe3c33
MacWilliam <874681044@qq.com>
2020-04-17 23:52:50 +0800

    color rgb

--- TrainAndTest/Fbank/ResNets/train_exres.sh
+++ TrainAndTest/Fbank/ResNets/train_exres.sh
@@ -23,7 +23,7 @@ if [ $stage -le 0 ]; then
       --model ExResNet34 \
       --resnet-size 34 \
       --feat-dim 64 \
-      --stride 1 \
+      --stride 2 \
       --kernel-size 5,5 \
       --batch-size 64 \
       --lr 0.1 \
