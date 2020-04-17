7ddf81abc14f033d1283b4a6ccfe7c262b414b54
MacWilliam <874681044@qq.com>
2020-04-18 00:41:56 +0800

    astdnn mfcc40

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 stage=1
+waited=0
+while [ `ps 45442 | wc -l` -eq 2 ]; do
+  sleep 60
+  waited=$(expr $waited + 1)
+  echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
+done
 
 if [ $stage -le 0 ]; then
   model=ETDNN
