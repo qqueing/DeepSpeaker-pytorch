304ef18d0e4699a72a0fc971ecb160f92c02b2f0
MacWilliam <874681044@qq.com>
2020-04-17 18:22:50 +0800

    color rgb

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -235,8 +235,8 @@ class ExporingResNet(nn.Module):
         self.layer4 = self._make_layer(block, num_filter[3], layers[3], stride=2)
 
         # [64, 128, 8, 37]
-        freq_dim = 1
-        time_dim = 2
+        freq_dim = 4
+        time_dim = 1
         self.avgpool = nn.AdaptiveAvgPool2d((freq_dim, time_dim))
         # 300 is the length of features
         # self.fc1 = nn.Linear(num_filter[3] * time_dim, embedding_size)
