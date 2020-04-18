e526d2a601e29f7c1ec11fd82a1cdebe11b8d521
MacWilliam <874681044@qq.com>
2020-04-18 10:04:42 +0800

    color rgb

--- TrainAndTest/Fbank/ResNets/train_exres.sh
+++ TrainAndTest/Fbank/ResNets/train_exres.sh
@@ -2,7 +2,7 @@
 
 stage=0
 waited=0
-while [ `ps 149046 | wc -l` -eq 2 ]; do
+while [ `ps 75486 | wc -l` -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
