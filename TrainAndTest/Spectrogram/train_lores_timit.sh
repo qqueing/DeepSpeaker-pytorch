0e9736663650a01f60bf784ba1d67e26d49b1398
MacWilliam <874681044@qq.com>
2020-04-19 12:43:47 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #stage=3
-stage=5
+stage=6
 
 if [ $stage -le 0 ]; then
   for loss in soft ; do
@@ -152,18 +152,38 @@ if [ $stage -le 5 ]; then
   done
 fi
 
-if [ $stage -le 5 ]; then
+
+if [ $stage -le 6 ]; then
+  datasets=libri
   for loss in soft ; do
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_spect_noc \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_spect_noc \
+      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/dev_spect_161 \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_spect_161 \
       --nj 14 \
       --epochs 15 \
       --lr 0.1 \
       --milestones 7,11 \
-      --check-path Data/checkpoint/LoResNet10/timit_spect/${loss}_fix \
-      --resume Data/checkpoint/LoResNet10/timit_spect/${loss}_fix/checkpoint_1.pth \
+      --check-path Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix \
+      --resume Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix/checkpoint_1.pth \
+      --channels 4,16,64 \
+      --statis-pooling \
+      --embedding-size 128 \
+      --input-per-spks 256 \
+      --num-valid 2 \
+      --weight-decay 0.001 \
+      --dropout-p 0.5 \
+      --loss-type ${loss}
+
+    python TrainAndTest/Spectrogram/train_lores10_var.py \
+      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/dev_spect_161 \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_spect_161 \
+      --nj 14 \
+      --epochs 15 \
+      --lr 0.1 \
+      --milestones 7,11 \
+      --check-path Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_var \
+      --resume Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_var/checkpoint_1.pth \
       --channels 4,16,64 \
       --statis-pooling \
       --embedding-size 128 \
@@ -174,4 +194,5 @@ if [ $stage -le 5 ]; then
       --loss-type ${loss}
   done
 fi
+
 exit 0;
