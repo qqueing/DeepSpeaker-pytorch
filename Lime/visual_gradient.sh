ad668dd1cc12130765266511600ac90bdf50f73d
MacWilliam <874681044@qq.com>
2020-04-21 12:50:23 +0800

    color rgb

--- Lime/visual_gradient.sh
+++ Lime/visual_gradient.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-stage=5
+stage=0
 if [ $stage -le 0 ]; then
   for model in LoResNet10 ; do
     python Lime/visual_gradient.py \
-      --extract-path Data/xvector/LoResNet10/timit/spect_161/soft/LoResNet10/soft_dp0.00/epoch_15 \
+      --extract-path Data/gradient/LoResNet10/timit/spect_161/soft_var/LoResNet10/soft_dp0.00/epoch_15 \
       --feat-dim 161
   done
 fi
 
+stage=10
 if [ $stage -le 1 ]; then
   for model in LoResNet10 ; do
     python Lime/visual_gradient.py \
