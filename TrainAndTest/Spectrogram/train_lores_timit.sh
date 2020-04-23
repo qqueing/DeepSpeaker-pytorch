0d47b8baf0c52719be12c8f823503c68422b49ef
MacWilliam <874681044@qq.com>
2020-04-24 01:28:17 +0800

    librispeech

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -157,27 +157,27 @@ if [ $stage -le 6 ]; then
   datasets=libri
   for loss in soft ; do
     echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/dev_spect_161 \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_spect_161 \
-      --nj 14 \
-      --epochs 15 \
-      --lr 0.1 \
-      --milestones 7,11 \
-      --check-path Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix \
-      --resume Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix/checkpoint_1.pth \
-      --channels 4,16,64 \
-      --statis-pooling \
-      --embedding-size 128 \
-      --input-per-spks 256 \
-      --num-valid 2 \
-      --weight-decay 0.001 \
-      --dropout-p 0.5 \
-      --loss-type ${loss}
+#    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
+#      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/dev_spect_161 \
+#      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_spect_161 \
+#      --nj 14 \
+#      --epochs 15 \
+#      --lr 0.1 \
+#      --milestones 7,11 \
+#      --check-path Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix \
+#      --resume Data/checkpoint/LoResNet10/${datasets}_spect/${loss}_fix/checkpoint_1.pth \
+#      --channels 4,16,64 \
+#      --statis-pooling \
+#      --embedding-size 128 \
+#      --input-per-spks 256 \
+#      --num-valid 2 \
+#      --weight-decay 0.001 \
+#      --dropout-p 0.5 \
+#      --loss-type ${loss}
 
     python TrainAndTest/Spectrogram/train_lores10_var.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/dev_spect_161 \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_spect_161 \
+      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_wcmvn \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_wcmvn \
       --nj 14 \
       --epochs 15 \
       --lr 0.1 \
