272935b0cb89d8d7f11c60d1c56ee317c3932eca
MacWilliam <874681044@qq.com>
2020-04-23 11:06:59 +0800

    color rgb

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=15
+stage=5
 if [ $stage -le 0 ]; then
   for loss in asoft soft ; do
     echo -e "\033[31m==> Loss type: ${loss} \033[0m"
@@ -17,15 +17,16 @@ fi
 
 if [ $stage -le 5 ]; then
   model=LoResNet10
+#  --resume Data/checkpoint/LoResNet10/spect/${loss}_dp25_128/checkpoint_24.pth \
   for loss in soft ; do
     echo -e "\033[31m==> Loss type: ${loss} \033[0m"
     python TrainAndTest/test_vox1.py \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test \
+      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev_wcmvn \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test_wcmvn \
       --nj 12 \
       --model ${model} \
       --embedding-size 128 \
-      --resume Data/checkpoint/LoResNet10/spect/${loss}_dp25_128/checkpoint_24.pth \
+      --resume Data/checkpoint/LoResNet10/spect/soft_wcmvn/checkpoint_24.pth \
       --loss-type soft \
       --num-valid 2 \
       --gpu-id 1
@@ -52,6 +53,7 @@ fi
 #    --lr 0.01 \
 #    --epochs 10
 
+stage=100
 if [ $stage -le 15 ]; then
   model=ASTDNN
   feat=fb40
