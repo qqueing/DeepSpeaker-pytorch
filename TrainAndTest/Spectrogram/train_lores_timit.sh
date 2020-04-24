e8a6243f112e04612d1a71ac16e93c5befd6d55f
MacWilliam <874681044@qq.com>
2020-04-24 22:53:52 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -70,9 +70,9 @@ if [ $stage -le 6 ]; then
       --epochs 15 \
       --lr 0.1 \
       --milestones 7,11 \
-      --check-path Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_128_0.25_8 \
-      --resume Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_128_0.25_8/checkpoint_1.pth \
-      --alpha 8 \
+      --check-path Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_128_0.25_0 \
+      --resume Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_128_0.25_0/checkpoint_1.pth \
+      --alpha 0 \
       --channels 4,32,128 \
       --embedding-size 128 \
       --input-per-spks 256 \
