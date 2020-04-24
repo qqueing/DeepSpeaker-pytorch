5d1756c5307da8f6947da7390ed2dcb3b3f670af
MacWilliam <874681044@qq.com>
2020-04-24 20:00:47 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -78,7 +78,7 @@ if [ $stage -le 6 ]; then
       --input-per-spks 256 \
       --num-valid 2 \
       --weight-decay 0.001 \
-      --dropout-p 0.25 \
+      --dropout-p 0.5 \
       --loss-type ${loss}
 
 #    python TrainAndTest/Spectrogram/train_lores10_var.py \
