9efbbb6a1d03570c8001e3063c6ea2595b2d0399
MacWilliam <874681044@qq.com>
2020-04-25 15:59:50 +0800

    color rgb

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -216,7 +216,7 @@ if [ $stage -le 9 ]; then
     python Process_Data/Compute_Feat/make_feat_kaldi.py \
       --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/${name} \
       --out-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit \
-      --out-set ${name}_fb24_dnn_mv \
+      --out-set ${name}_fb24_dnn_m \
       --filter-type dnn.timit \
       --feat-type fbank \
       --nfft 320 \
