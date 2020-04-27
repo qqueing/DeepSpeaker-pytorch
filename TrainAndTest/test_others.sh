17ff2e7fe2f79e1f93edeea8456d2ef0a35c1564
MacWilliam <874681044@qq.com>
2020-04-27 16:24:55 +0800

    color rgb

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=5
+stage=30
 if [ $stage -le 0 ]; then
   for loss in asoft soft ; do
     echo -e "\033[31m==> Loss type: ${loss} \033[0m"
@@ -54,7 +54,7 @@ fi
 #    --lr 0.01 \
 #    --epochs 10
 
-stage=200
+#stage=200
 if [ $stage -le 15 ]; then
   model=ASTDNN
   feat=fb40
@@ -108,7 +108,7 @@ if [ $stage -le 20 ]; then
   done
 fi
 
-stage=250
+#stage=250
 if [ $stage -le 25 ]; then
   model=LoResNet10
   feat=spect_wcmvn
@@ -139,6 +139,27 @@ if [ $stage -le 25 ]; then
       --resume Data/checkpoint/LoResNet10/timit_spect/soft_var/checkpoint_15.pth \
       --loss-type soft \
       --dropout-p 0.25 \
+      --num-valid 2 \
+      --gpu-id 1
+  done
+fi
+
+
+if [ $stage -le 30 ]; then
+  model=ResNet20
+  feat=spect_wcmvn
+  datasets=vox
+  for loss in soft ; do
+    echo -e "\033[31m==> Loss type: ${loss} variance_fix length \033[0m"
+    python TrainAndTest/test_vox1.py \
+      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev_257_wcmvn \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test_257_wcmvn \
+      --nj 12 \
+      --model ${model} \
+      --embedding-size 128 \
+      --resume Data/checkpoint/ResNet20/spect_257_wcmvn/soft_dp0.5/checkpoint_24.pth \
+      --loss-type soft \
+      --dropout-p 0.5 \
       --num-valid 2 \
       --gpu-id 1
   done
