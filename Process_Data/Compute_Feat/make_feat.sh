31c601197713cc83c16981ff808535cb9676d452
MacWilliam <874681044@qq.com>
2020-04-27 22:50:46 +0800

    train timit

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=1
+stage=9
 # voxceleb1
 if [ $stage -le 0 ]; then
   for name in dev test ; do
@@ -73,7 +73,7 @@ if [ $stage -le 1 ]; then
   done
 fi
 
-stage=100
+#stage=100
 
 if [ $stage -le 1 ]; then
   for name in dev test ; do
@@ -235,8 +235,8 @@ if [ $stage -le 9 ]; then
     python Process_Data/Compute_Feat/make_feat_kaldi.py \
       --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/${name} \
       --out-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb \
-      --out-set ${name}_dfb24_mdv \
-      --filter-type dnn.timit.mdv \
+      --out-set ${name}_dfb24_vart \
+      --filter-type dnn.timit.var \
       --feat-type fbank \
       --nfft 320 \
       --windowsize 0.02 \
