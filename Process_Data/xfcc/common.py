6ef3e4e14b10c38685d7ed63a561ab4a7ebc7590
MacWilliam <874681044@qq.com>
2020-04-28 12:02:48 +0800

    color rgb

--- Process_Data/xfcc/common.py
+++ Process_Data/xfcc/common.py
@@ -138,11 +138,11 @@ def get_filterbanks(nfilt=20, nfft=512, samplerate=16000, lowfreq=0,
             num_wei = 0.
             for i in range(nfft // 2 + 1):
                 num_wei += weight[i]
-                if num_wei < (j + 1) / (nfilt + 2):
-                    continue
-                else:
+                if num_wei > (j + 1) / (nfilt + 2):
                     bin.append(i)
                     break
+                else:
+                    continue
 
         bin.append(highfreq_idx[-1])
     else:
