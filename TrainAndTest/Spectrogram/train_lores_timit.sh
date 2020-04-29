ffd7f6ff2bdc234e71cb00e27b75826161167828
MacWilliam <874681044@qq.com>
2020-04-29 17:26:10 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -85,18 +85,36 @@ if [ $stage -le 6 ]; then
 #      --dropout-p 0.25 \
 #      --loss-type ${loss}
 
-    echo -e "\n\033[1;4;31m Training var length with ${loss}\033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_var.py \
-      --model ${model} \
+#    echo -e "\n\033[1;4;31m Training var length with ${loss}\033[0m\n"
+#    python TrainAndTest/Spectrogram/train_lores10_var.py \
+#      --model ${model} \
+#      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
+#      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/test_noc \
+#      --nj 14 \
+#      --epochs 15 \
+#      --lr 0.1 \
+#      --milestones 7,11 \
+#      --check-path Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_var_24 \
+#      --resume Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_var_24/checkpoint_1.pth \
+#      --channels 4,32,128 \
+#      --statis-pooling \
+#      --embedding-size 128 \
+#      --input-per-spks 256 \
+#      --num-valid 1 \
+#      --weight-decay 0.001 \
+#      --dropout-p 0.25 \
+#      --loss-type ${loss}
+    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
+      --model LoResNet10 \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/test_noc \
+      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
       --nj 14 \
-      --epochs 15 \
+      --epochs 14 \
       --lr 0.1 \
       --milestones 7,11 \
-      --check-path Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_var_24 \
-      --resume Data/checkpoint/LoResNet10/${datasets}/spect/${loss}_var_24/checkpoint_1.pth \
-      --channels 4,32,128 \
+      --check-path Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss} \
+      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}/checkpoint_1.pth \
+      --channels 4,16,64 \
       --statis-pooling \
       --embedding-size 128 \
       --input-per-spks 256 \
