df658e28ee94fef46e69162c8ef63d9769507233
MacWilliam <874681044@qq.com>
2020-04-29 20:10:03 +0800

    color rgb

--- TrainAndTest/Spectrogram/train_lores_timit.sh
+++ TrainAndTest/Spectrogram/train_lores_timit.sh
@@ -60,7 +60,7 @@ if [ $stage -le 6 ]; then
   model=LoResNet10
 #  --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_wcmvn \
 #  --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_wcmvn \
-  for loss in soft ; do
+  for loss in amsoft center ; do
 #    echo -e "\n\033[1;4;31m Training with ${loss}\033[0m\n"
 #    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
 #      --model ${model} \
@@ -104,44 +104,64 @@ if [ $stage -le 6 ]; then
 #      --weight-decay 0.001 \
 #      --dropout-p 0.25 \
 #      --loss-type ${loss}
+#    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
+#      --model LoResNet10 \
+#      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
+#      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
+#      --nj 14 \
+#      --epochs 15 \
+#      --lr 0.1 \
+#      --milestones 7,11 \
+#      --check-path Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss} \
+#      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}/checkpoint_1.pth \
+#      --channels 4,16,64 \
+#      --statis-pooling \
+#      --alpha 9.8 \
+#      --embedding-size 128 \
+#      --input-per-spks 256 \
+#      --num-valid 1 \
+#      --weight-decay 0.001 \
+#      --dropout-p 0.25 \
+#      --loss-type ${loss}
+#
+#    python TrainAndTest/Spectrogram/train_lores10_var.py \
+#      --model LoResNet10 \
+#      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
+#      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
+#      --nj 14 \
+#      --epochs 15 \
+#      --lr 0.1 \
+#      --milestones 7,11 \
+#      --check-path Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}_var \
+#      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}_var/checkpoint_1.pth \
+#      --channels 4,16,64 \
+#      --alpha 9.8 \
+#      --statis-pooling \
+#      --embedding-size 128 \
+#      --input-per-spks 256 \
+#      --num-valid 1 \
+#      --weight-decay 0.001 \
+#      --dropout-p 0.25 \
+#      --loss-type ${loss}
     python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --model LoResNet10 \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
       --nj 14 \
-      --epochs 15 \
-      --lr 0.1 \
-      --milestones 7,11 \
+      --epochs 10 \
+      --lr 0.01 \
+      --milestones 6 \
       --check-path Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss} \
-      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}/checkpoint_1.pth \
+      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/soft/checkpoint_15.pth \
       --channels 4,16,64 \
-      --statis-pooling \
+      --finetune \
       --alpha 9.8 \
       --embedding-size 128 \
       --input-per-spks 256 \
       --num-valid 1 \
       --weight-decay 0.001 \
       --dropout-p 0.25 \
-      --loss-type ${loss}
-
-    python TrainAndTest/Spectrogram/train_lores10_var.py \
-      --model LoResNet10 \
-      --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
-      --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/libri/spect/dev_noc \
-      --nj 14 \
-      --epochs 15 \
-      --lr 0.1 \
-      --milestones 7,11 \
-      --check-path Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}_var \
-      --resume Data/checkpoint/LoResNet10/${datasets}/spect_noc/${loss}_var/checkpoint_1.pth \
-      --channels 4,16,64 \
-      --alpha 9.8 \
-      --statis-pooling \
-      --embedding-size 128 \
-      --input-per-spks 256 \
-      --num-valid 1 \
-      --weight-decay 0.001 \
-      --dropout-p 0.25 \
+      --veri-pairs 15000 \
       --loss-type ${loss}
   done
 fi
