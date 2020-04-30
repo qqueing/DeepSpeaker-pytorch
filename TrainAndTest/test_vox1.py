2260768669be8eb667f0f23f9acabc1a865679c6
MacWilliam <874681044@qq.com>
2020-04-30 23:42:45 +0800

    color rgb

--- TrainAndTest/test_vox1.py
+++ TrainAndTest/test_vox1.py
@@ -500,3 +500,7 @@ def sitw_test(sitw_test_loader, model, epoch):
 
 if __name__ == '__main__':
     main()
+
+# test hard spectrogram soft 161 vox1
+#   Test ERR is 2.9182%, Threshold is 0.35036733746528625
+#   mindcf-0.01 0.3369, mindcf-0.001 0.5494.
