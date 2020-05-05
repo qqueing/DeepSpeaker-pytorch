742c174c7e03b581c6db615146c83fe5a67b2140
MacWilliam <874681044@qq.com>
2020-05-05 20:39:32 +0800

    color rgb

--- Lime/visual_gradient.sh
+++ Lime/visual_gradient.sh
@@ -75,7 +75,7 @@ if [ $stage -le 30 ]; then
 #      --acoustic-feature fbank
 
     python Lime/visual_gradient.py \
-      --extract-path Data/gradient/ExResNet34/vox1/fb64_wcmvn/soft_11/epoch_22 \
+      --extract-path Data/gradient/ExResNet34/vox1/fb64_wcmvn/soft_fix/epoch_20 \
       --feat-dim 64 \
       --acoustic-feature fbank
 
