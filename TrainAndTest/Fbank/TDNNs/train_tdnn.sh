aa7b742182a947ba85acd048b0f8ccf2389c1f1e
MacWilliam <874681044@qq.com>
2020-05-06 20:21:25 +0800

    color rgb

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -37,7 +37,7 @@ if [ $stage -le 5 ]; then
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_pyfb/dev_fb40_wcmvn \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_pyfb/test_fb40_wcmvn \
       --check-path Data/checkpoint/${model}/${feat}/${loss} \
-      --resume Data/checkpoint/${model}/${feat}/${loss}/checkpoint_1.pth \
+      --resume Data/checkpoint/${model}/${feat}/${loss}/checkpoint_2.pth \
       --batch-size 80 \
       --remove-vad \
       --epochs 18 \
