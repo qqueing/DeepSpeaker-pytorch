564e3c5f442f5e2771d0b92b6dbc6b37b9c69373
MacWilliam <874681044@qq.com>
2020-05-07 13:34:52 +0800

    color rgb

--- TrainAndTest/test_vox1.py
+++ TrainAndTest/test_vox1.py
@@ -33,7 +33,7 @@ from Define_Model.SoftmaxLoss import AngleLinear, AdditiveMarginLinear
 from Define_Model.model import PairwiseDistance
 from Process_Data.KaldiDataset import ScriptTrainDataset, ScriptValidDataset, KaldiExtractDataset, \
     ScriptVerifyDataset
-from Process_Data.audio_processing import to2tensor, varLengthFeat, concateinputfromMFB
+from Process_Data.audio_processing import to2tensor, varLengthFeat, concateinputfromMFB, totensor
 from TrainAndTest.common_func import create_model
 from eval_metrics import evaluate_kaldi_eer, evaluate_kaldi_mindcf
 from logger import NewLogger
@@ -208,11 +208,11 @@ if args.input_length == 'var':
 elif args.input_length == 'fix':
     transform = transforms.Compose([
         concateinputfromMFB(remove_vad=args.remove_vad),
-        to2tensor()
+        totensor()
     ])
     transform_T = transforms.Compose([
         concateinputfromMFB(input_per_file=args.test_input_per_file, remove_vad=args.remove_vad),
-        to2tensor()
+        totensor()
     ])
 else:
     raise ValueError('input length must be var or fix.')
