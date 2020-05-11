ff8581801144fa73e5805d5e079046fbe174fcb2
MacWilliam <874681044@qq.com>
2020-05-11 17:25:16 +0800

    color rgb

--- TrainAndTest/Fbank/ResNets/train_sires34_kaldi.py
+++ TrainAndTest/Fbank/ResNets/train_sires34_kaldi.py
@@ -21,6 +21,7 @@ import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torchvision.transforms as transforms
+from kaldi_io import read_mat
 from tensorboardX import SummaryWriter
 from torch.autograd import Variable
 from torch.optim.lr_scheduler import MultiStepLR
