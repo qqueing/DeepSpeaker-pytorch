96923d6b14b1f6beb563888a6a3435d2af2b4579
MacWilliam <874681044@qq.com>
2020-05-13 20:52:47 +0800

    lores-10

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=1
+stage=15
 
 waited=0
 while [ `ps 113458 | wc -l` -eq 2 ]; do
@@ -140,91 +140,57 @@ if [ $stage -le 7 ]; then
   done
 fi
 
-if [ $stage -le 8 ]; then
-  for loss in soft ; do
-    echo -e "\n\033[1;4;31m Training with ${loss} kernel 3x3\033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --nj 12 \
-      --epochs 12 \
-      --milestones 4,8 \
-      --resnet-size 10 \
-      --check-path Data/checkpoint/LoResNet10/spectrogram/${loss}_64 \
-      --resume Data/checkpoint/LoResNet10/spectrogram/${loss}_64/checkpoint_24.pth \
-      --channels 64,128,256,512 \
-      --kernel-size 3,3 \
-      --loss-type ${loss} \
-      --lr 0.01 \
-      --num-valid 2 \
-      --dropout-p 0.5
-  done
-fi
-
-
-#stage=11
-if [ $stage -le 8 ]; then
-  for loss in asoft amsoft center ; do
-    echo -e "\n\033[1;4;31m Training with ${loss} kernel 3x3\033[0m\n"
-    python TrainAndTest/Spectrogram/train_lores10_kaldi.py \
-      --nj 12 \
-      --epochs 24 \
-      --milestones 10,15,20 \
-      --resnet-size 10 \
-      --check-path Data/checkpoint/LoResNet10/spectrogram/${loss} \
-      --resume Data/checkpoint/LoResNet10/spectrogram/${loss}/checkpoint_20.pth \
-      --channels 64,128,256,512 \
-      --kernel-size 3,3 \
-      --loss-type ${loss} \
-      --margin 0.35 \
-      --s 30 \
-      --m 4 \
-      --num-valid 2 \
-      --dropout-p 0.25
-  done
-fi
 
 if [ $stage -le 15 ]; then
 #  for loss in soft ; do # 32,128,512; 8,32,128
   for loss in soft ; do # 32,128,512; 8,32,128
     echo -e "\n\033[1;4;31m Training with ${loss} kernel 5x5\033[0m\n"
-    python -W ignore TrainAndTest/Spectrogram/train_lores10_var.py \
+    python -W ignore TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --model LoResNet10 \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test \
+      --input-per-spks 224 \
       --nj 12 \
       --epochs 24 \
-      --resnet-size 8 \
+      --resnet-size 10 \
       --embedding-size 128 \
+      --kernel-size 3,3 \
       --milestones 10,15,20 \
-      --channels 64,128,256 \
-      --check-path Data/checkpoint/LoResNet10/spect/${loss}_dp25_128_var \
-      --resume Data/checkpoint/LoResNet10/spect/${loss}_dp25_128_var/checkpoint_20.pth \
+      --channels 64,128,256,512 \
+      --check-path Data/checkpoint/LoResNet10/spect/${loss}_dp25 \
+      --resume Data/checkpoint/LoResNet10/spect/${loss}_dp25/checkpoint_20.pth \
       --loss-type ${loss} \
       --lr 0.1 \
       --num-valid 2 \
+      --gpu-id 1 \
       --dropout-p 0.25
   done
 
-  for loss in asoft amsoft center ; do # 32,128,512; 8,32,128
+  for loss in amsoft center asoft ; do # 32,128,512; 8,32,128
     echo -e "\n\033[1;4;31m Training with ${loss} kernel 5x5\033[0m\n"
     python -W ignore TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --model LoResNet10 \
       --train-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/dev \
       --test-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/Vox1_spect/test \
+      --input-per-spks 224 \
       --nj 12 \
-      --epochs 24 \
+      --epochs 12 \
       --resnet-size 8 \
       --embedding-size 128 \
-      --milestones 10,15,20 \
-      --channels 64,128,256 \
-      --check-path Data/checkpoint/LoResNet10/spect/${loss}_dp25_128 \
-      --resume Data/checkpoint/LoResNet10/spect/${loss}_dp25_128/checkpoint_20.pth \
+      --kernel-size 3,3 \
+      --milestones 6,10 \
+      --channels 64,128,256,512 \
+      --check-path Data/checkpoint/LoResNet10/spect/${loss}_dp25 \
+      --resume Data/checkpoint/LoResNet10/spect/soft_dp25/checkpoint_24.pth \
       --loss-type ${loss} \
-      --lr 0.1 \
+      --finetune \
+      --lr 0.01 \
       --num-valid 2 \
-      --margin 0.3 \
-      --s 20 \
-      --m 3 \
-      --loss-ratio 0.001 \
+      --margin 0.35 \
+      --s 15 \
+      --m 4 \
+      --loss-ratio 0.01 \
+      --gpu-id 1 \
       --dropout-p 0.25
   done
 fi
